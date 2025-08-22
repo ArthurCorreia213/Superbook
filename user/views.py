a154d844ef46eeaa620ee68ffde2ad22fe009009
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2025-08-22 16:49:11 -0300

    Formulário funcionando porém falta as migrations ent n tem db então n vai :)

--- user/views.py
+++ user/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 from .forms import UserRegisterForm
 
-from django.contrib.auth.forms import UserCreationForm
-
 # Create your views here.
 
 def register(request):
-    form = UserRegisterForm()
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            print(f"Conta criada para user {username}")
+        else:
+            print("FORMULARIO INVALIDO")
+            print(form)
+            print(form.errors)
+            print(form.error_messages)
+    else:
+        form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
